cc22dc0242851408d842c0d5398f2be4549d9362
Tamako Aiba <t.aiba@4nonome.com>
2021-06-06 17:15:04 +0900

    リストできた。

---
+++ Original/Count.swift
@@ -0,0 +1,13 @@
+//
+//  Count.swift
+//  Original
+//
+//  Created by tmk_cheer on 2021/06/05.
+//
+
+import Foundation
+import RealmSwift
+
+class Count: Object {
+    @objc dynamic var count: Int = 0
+}
